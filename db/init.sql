3eec996b5ae921f1e83a8d4e8117a3ecb2b8152b
Valerie Yip <valerieyipfh@gmail.com>
2021-08-22 13:50:21 -0400

    fixed typo in schema

--- db/init.sql
+++ db/init.sql
@@ -17,16 +17,16 @@ CREATE TABLE stock_price (
     CONSTRAINT fk_stock FOREIGN KEY (stock_id) REFERENCES stock (id)
 );
 
-CREATE TABLE submissions (
+CREATE TABLE submission (
     submission_id INTEGER PRIMARY KEY NOT NULL,
     dt TIMESTAMP WITHOUT TIME ZONE NOT NULL,
     selftext TEXT,
     title TEXT NOT NULL,
     stock_id INTEGER,
     CONSTRAINT fk_submission_stock FOREIGN KEY (stock_id) REFERENCES stock (id)
-)
+);
 
-CREATE TABLE comments (
+CREATE TABLE comment (
     comment_id INTEGER PRIMARY KEY NOT NULL,
     dt TIMESTAMP WITHOUT TIME ZONE NOT NULL,
     body TEXT NOT NULL,
@@ -35,7 +35,7 @@ CREATE TABLE comments (
     submission_id INTEGER,
     CONSTRAINT fk_comment_body_stock FOREIGN KEY (stock_id_body) REFERENCES stock (id),
     CONSTRAINT fk_comment_submission_stock FOREIGN KEY (submission_id) REFERENCES submission (submission_id)
-)
+);
 
 CREATE TABLE mention (
     stock_id INTEGER,
