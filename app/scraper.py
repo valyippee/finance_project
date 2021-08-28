9c98829cf7dbe6703330ca4c4cc29733bdd791af
valyippee <valerieyipfh@gmail.com>
2021-08-28 19:01:27 -0400

    finished scraping stock data function

--- app/scraper.py
+++ app/scraper.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import praw
 import config
 import requests
@@ -6,7 +8,10 @@ from db.stock_repository import StockRepository
 from base_db import Stock
 
 
-LEGAL_ELEMENTS = ["Ltd.", "Corp.", "Corp", "Corporation", "Inc.", "Incorporated"]
+LEGAL_ELEMENTS = ["Ltd.", "Corp.", "Corp", "Corporation", "Inc.", "Inc", "Incorporated", "Plc"]
+TO_DELETE = ["Services", "Capital", "Holdings", "Holding", "Investment Trust",
+             "Realty Trust", "Property Trust", "Properties Trust"]
+TO_DELETE2 = ["Hotels & Resorts", "Systems", "Properties", "Technologies", "Group"]
 
 
 class RedditScraper:
@@ -68,23 +73,52 @@ class StockScraper:
 
                 # filter out name_variations of the stock
                 name_variations = [stock_info[0], stock_info[1]]
-                shortform_name = ""
-                for element in LEGAL_ELEMENTS:
-                    if element in stock_info[1]:
-                        name_parts = stock_info[1].split(element)
-                        shortform_name = name_parts[0].strip()
-                        if shortform_name[-1] == ",":
-                            shortform_name = shortform_name[0:len(shortform_name) - 1]
-                        break
-                if shortform_name != "":
-                    name_variations.append(shortform_name)
+                name_variations.extend(self._form_name_variations(stock_info[1]))
 
                 new_stock = Stock(symbol=stock_info[0],
                                   name=stock_info[1],
                                   exchange=exchange,
                                   name_variations=name_variations)
-                print(new_stock.name_variations)
-                # self._stock_repository.input_stock(new_stock)
+                self._stock_repository.input_stock(new_stock)
+
+    def _form_name_variations(self, company_name: str) -> List:
+        """
+        Given a company's name, return a list of name variations to be stored in the database.
+        """
+        name_variations = []
+        shortform_name = ""
+        for element in LEGAL_ELEMENTS:
+            if element in company_name:
+                name_parts = company_name.split(element)
+                shortform_name = name_parts[0].strip()
+                if len(shortform_name) > 0 and shortform_name[-1] == ",":
+                    shortform_name = shortform_name[0:len(shortform_name) - 1]
+                break
+        if shortform_name == "":
+            return []
+        else:
+            name_variations.append(shortform_name)
+
+        # further break down the names
+        if "Technologies" in shortform_name:
+            new_name = shortform_name.replace("Technologies", "Tech")
+            name_variations.append(new_name)
+        elif "Group" in shortform_name:
+            new_name = shortform_name.replace("Group", "Grp")
+            name_variations.append(new_name)
+        elif "Hotels & Resorts" in shortform_name:
+            new_name = shortform_name.replace("Hotels & Resorts", "Hotels")
+            name_variations.append(new_name)
+
+        for name_to_delete in TO_DELETE:
+            if name_to_delete in shortform_name:
+                new_name_parts = shortform_name.split(name_to_delete)
+                name_variations.append(new_name_parts[0].strip())
+        for name_to_delete in TO_DELETE2:
+            if name_to_delete in shortform_name:
+                new_name_parts = shortform_name.split(name_to_delete)
+                name_variations.append(new_name_parts[0].strip())
+        return name_variations
 
 
 if __name__ == "__main__":
