5fd44e81db3c9292a25d489cdf8d59f5b09966c4
valyippee <valerieyipfh@gmail.com>
2021-08-28 11:49:48 -0400

    added delete stock functions

--- db/stock_repository.py
+++ db/stock_repository.py
@@ -1,3 +1,5 @@
+from typing import List, Optional
+
 from base_db import engine, Stock
 from sqlalchemy.orm import sessionmaker
 
@@ -15,10 +17,45 @@ class StockRepository:
 
     def input_stock(self, new_stock: Stock):
         """
-        Inputs stock into stock table
+        Inputs stock into stock table.
 
-        Precondition: this stock belongs to a company, not an etf
+        Precondition: this stock belongs to a company, not an etf.
         """
         with self.Session() as session:
             session.add(new_stock)
             session.commit()
+
+    def find_by_id(self, stock_id: int) -> Optional[Stock]:
+        """
+        Returns a Stock based on id, or None if the stock does not exist.
+        """
+        session = self.Session()
+        result = session.query(Stock).get(stock_id)
+        session.close()
+        return result
+
+    def find_all(self) -> List[Stock]:
+        """
+        Returns all Stock in the database in a list.
+        """
+        session = self.Session()
+        result = session.query(Stock).all()
+        session.close()
+        return result
+
+    def delete_by_id(self, stock_id: int) -> None:
+        """
+        Deletes a Stock based on id, if the Stock is in the database.
+        Deletes data from other tables as well to maintain foreign key constraint.
+        """
+        with self.Session() as session:
+            session.query(Stock).filter(Stock.id == stock_id).delete()
+            session.commit()
+
+    def delete_all(self) -> None:
+        """
+        Deletes all Stock in the database
+        Deletes data from other tables as well to maintain foreign key constraint.
+        """
+        with self.Session() as session:
+            session.query(Stock).delete()
--- test_db/test_stock_repository.py
+++ test_db/test_stock_repository.py
@@ -8,8 +8,33 @@ from test_db.base_test_db import Stock, engine
 
 def test_input_stock():
     stock_repository = StockRepository(engine)
-    sample_stock = Stock(symbol="123", name="sample", exchange="NYSE")
+    sample_stock = Stock(symbol="234", name="sample2", exchange="NYSE")
     stock_repository.input_stock(sample_stock)
 
 
-test_input_stock()
+def test_find_stock_by_id():
+    stock_repository = StockRepository(engine)
+    returned_stock = stock_repository.find_by_id(1)
+    assert returned_stock.name == "sample"
+    assert returned_stock.exchange == "NYSE"
+
+
+def test_find_all():
+    stock_repository = StockRepository(engine)
+    result = stock_repository.find_all()
+    for stock in result:
+        print(stock.id)
+
+
+def test_delete_by_id():
+    stock_repository = StockRepository(engine)
+    stock_repository.delete_by_id(2)
+    print(stock_repository.find_by_id(2))
+
+
+def test_delete_all():
+    stock_repository = StockRepository(engine)
+    assert stock_repository.find_all() is None
+
+
+test_delete_by_id()
