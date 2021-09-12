1c0599d017ae8a30509d5e5c475554e5b29dec3f
valyippee <valerieyipfh@gmail.com>
2021-09-12 18:38:51 -0400

    added get_live_comments in reddit scraper

--- main_app/app.py
+++ main_app/app.py
@@ -1,6 +1,7 @@
 import logging.config
 import yaml
 import os
+from base_test_db import engine
 from reddit_scraper import RedditScraper
 
 log_dir = os.path.join(os.path.normpath(os.getcwd() + os.sep + os.pardir), 'logs')
@@ -12,18 +13,7 @@ with open(log_config_fname, 'r') as f:
     config['handlers']['file']['filename'] = log_fname
     logging.config.dictConfig(config)
 
-# from base_test_db import engine
-# from reddit_scraper import RedditScraper
-#
-#
-# def test_count_mention():
-#     scraper = RedditScraper("wallstreetbets", engine)
-#     scraper.count_mentions_and_populate_table("This was exactly me 2 weeks ago. Loaded my IRA up with GME. "
-#                                               "It dropped 6% no more than 10 min after I bought em’ all. "
-#                                               "A few days later, was up 25%. Still holding, and hoping the same "
-#                                               "gains for you!",
-#                                               "2020-08-30 8:30 PM",
-#                                               "eitlom",
-#                                               False)
-#
-# test_count_mention()
+
+scraper = RedditScraper("wallstreetbets", engine)
+
+scraper.get_live_comments()
--- test_db/test_reddit_scraper.py
+++ test_db/test_reddit_scraper.py
@@ -4,7 +4,7 @@ from reddit_scraper import RedditScraper
 
 def test_count_mention():
     scraper = RedditScraper("wallstreetbets", engine)
-    scraper.count_mentions_and_populate_table("This was exactly me 2 weeks ago. Loaded my IRA up with GME. "
+    scraper.count_mentions_and_populate_table("XYZ was exactly me 2 weeks ago. Loaded my IRA up with GME. "
                                               "It dropped 6% no more than 10 min after I bought em’ all. "
                                               "A few days later, was up 25%. Still holding, and hoping the same "
                                               "gains for you!",
